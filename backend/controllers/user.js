6a0524c4af8b89557fc49ce4bb1ca8c1da787cbf
Laetitia FORGEUR <l.forgeur@gmail.com>
2021-09-06 12:35:34 +0200

    Ajout de la key du token dans dotenv

--- backend/controllers/user.js
+++ backend/controllers/user.js
@@ -42,7 +42,7 @@ exports.login = (req, res, next) => {
           }
           res.status(200).json({
             userId: user._id,
-            token: jwt.sign({ userID: user._id }, "RANDOM_TOKEN_SECRET", {
+            token: jwt.sign({ userID: user._id }, process.env.SECRET_KEY, {
               expiresIn: "24h",
             }),
           });
--- backend/middleware/auth.js
+++ backend/middleware/auth.js
@@ -3,7 +3,7 @@ const jwt = require("jsonwebtoken");
 module.exports = (req, res, next) => {
   try {
     const token = req.headers.authorization.split(" ")[1];
-    const decodedToken = jwt.verify(token, "RANDOM_TOKEN_SECRET");
+    const decodedToken = jwt.verify(token, process.env.SECRET_KEY);
     const userId = decodedToken.userId;
     // Si le userId de la requête ne correspond pas à celui du token => Invalid user ID sinon on appelle next()
     if (req.body.userId && req.body.userId !== userId) {
