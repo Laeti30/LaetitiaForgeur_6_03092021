0be71a715c0be0f1abace3229a410d284d5b0db8
Laetitia FORGEUR <l.forgeur@gmail.com>
2021-09-07 13:42:24 +0200

    Intégration email-validator

--- backend/controllers/user.js
+++ backend/controllers/user.js
@@ -1,26 +1,31 @@
 const bcrypt = require("bcrypt");
 const jwt = require("jsonwebtoken");
+const emailValidator = require("email-validator");
 
 const User = require("../models/User");
 
 exports.signup = (req, res, next) => {
-  bcrypt
-    // On hache le mot de passe 10 fois
-    .hash(req.body.password, 10)
-    .then((hash) => {
-      // On crée un nouvel utilisateur en utilisant notre schéma mongoose
-      const user = new User({
-        email: req.body.email,
-        // On récupère le mot de passe haché
-        password: hash,
-      });
-      // On sauvegarde ce nouvel utilisateur en bdd
-      user
-        .save()
-        .then(() => res.status(201).json({ message: "User created !" }))
-        .catch((error) => res.status(400).json({ error }));
-    })
-    .catch((error) => res.status(500).json({ error }));
+  // On vérifie l'adresse mail
+  if (emailValidator.validate(req.body.email)) {
+    bcrypt
+      .hash(req.body.password, 10) // On hache le mot de passe 10 fois
+      .then((hash) => {
+        // On crée un nouvel utilisateur en utilisant notre schéma mongoose
+        const user = new User({
+          email: req.body.email,
+          // On récupère le mot de passe haché
+          password: hash,
+        });
+        // On sauvegarde ce nouvel utilisateur en bdd
+        user
+          .save()
+          .then(() => res.status(201).json({ message: "User created !" }))
+          .catch((error) => res.status(400).json({ error }));
+      })
+      .catch((error) => res.status(500).json({ error }));
+  } else {
+    res.status(400).json({ message: "Merci de saisir un email valide" });
+  }
 };
 
 exports.login = (req, res, next) => {
--- backend/middleware/passwordChecker.js
+++ backend/middleware/passwordChecker.js
@@ -2,12 +2,10 @@ const passwordSchema = require("../models/Password");
 
 module.exports = (req, res, next) => {
   if (!passwordSchema.validate(req.body.password)) {
-    res
-      .status(400)
-      .json({
-        message:
-          "Le mot de passe doit contenir au min 10 carcatères dont au moins un chiffre, une majuscule, une minuscule et pas d'espace",
-      });
+    res.status(400).json({
+      message:
+        "Le mot de passe doit contenir au min 10 caractères dont au moins un chiffre, une majuscule, une minuscule et pas d'espace",
+    });
   } else {
     next();
   }
